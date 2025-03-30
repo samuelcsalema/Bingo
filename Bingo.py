ea8bab0a7a594481b3d7d0ab8105ff4048f5d33c
Samuel <samuelcsalema2@gmail.com>
2025-03-30 12:39:32 -0300

    modified:   Bingo.py

--- Bingo.py
+++ Bingo.py
@@ -1,20 +1,26 @@
+# Importações necessárias 
 import random
 import pandas as pd
-
+# Criando a função que gera a tabela
 def gerar_bingo():
+    # Range dos números na tabela (0 até 100)
     numeros = list(range(101))
+    # Randomizando a ordem dos números
     random.shuffle(numeros)
-
+    # Criando a tabela do bingo (lista)
     bingo = []
-    for i in range(5):
-        linha = numeros[i*5:(i+1)*5]
+    # Adicionando os números da tabela em 5 listas de 5 números
+    for numero in range(5):
+        linha = numeros[numero*5:(numero+1)*5]
         bingo.append(linha)
-
+    # Convertendo a lista em cartilha (DataFrame)
     df_bingo = pd.DataFrame(bingo, columns=["B", "I", "N", "G", "O"])
+    # Substituindo o valor central por nulo (X)
     df_bingo.iloc[2,2] = "X"
+    # Retornando a tabela do bingo completa
     return df_bingo
-
+# Gerando tabelas
 bingo1 = gerar_bingo()
 bingo2 = gerar_bingo()
-
-bingo1, bingo2
+# Exibindo as tabelas
+print(bingo1, '\n', bingo2)
